b3b89acbf96b06b3537f07b5be4444d622a67365
Dominic Barnes <dbarnes@treetopllc.com>
2013-09-05 17:27:19 -0500

    correcting static middleware, it was not able to serve assets in it's previous state

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var path = require("path");
 var express = require('express');
 var builder = require('./builder.js');
 
@@ -5,11 +6,11 @@ module.exports = function(opts) {
   var router = express();
 
   opts.dev = 'dev' in opts ? opts.dev : true;
-  opts.out = router.path().substr(1);
-
-  router.use(express.static(opts.root));
+  opts.out = opts.out || "build";
 
   var rebuild = builder(opts);
+
+  router.use(express.static(path.join(opts.root, opts.out)));
 
   router.get('/build.js', rebuild, function(req, res) {
     res.type('js');
